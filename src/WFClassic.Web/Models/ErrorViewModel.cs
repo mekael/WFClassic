efe149dbc2497175e0626b07d464e6264079cf9f
Michael Hannigan <michael.vincent.hannigan@gmail.com>
2025-04-07 00:20:22 -0500

    feat: Add initial project and solution.
    Add new .net project which uses individual auth.

---
+++ src/WFClassic.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WFClassic.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
