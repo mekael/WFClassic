759639d4255ac6dabcc42c7041becb0e7bdcdf1a
Michael Hannigan <michael.vincent.hannigan@gmail.com>
2025-05-04 21:43:22 -0500

    feat: simplify login verification
    Add a filter which can be applied at the method or controller level which uses the accountId and nonce provided in the query string to centralize verification rather than having it in each handler

---
+++ src/WFClassic.Web/Logic/Middleware/LoginVerificationActionFilter.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+using WFClassic.Web.Data;
+using WFClassic.Web.Logic.Admin.CheckOnline;
+using WFClassic.Web.Logic.Inventory.Get;
+
+namespace WFClassic.Web.Logic.Middleware
+{
+    public class LoginVerificationActionFilter : ActionFilterAttribute
+    {
+
+        private ILogger<LoginVerificationActionFilter> _logger;
+        private IsUserOnlineQueryHandler _isUserOnlineQueryHandler;
+
+        public LoginVerificationActionFilter(ILogger<LoginVerificationActionFilter> logger, IsUserOnlineQueryHandler isUserOnlineQueryHandler ) 
+        {
+            _isUserOnlineQueryHandler = isUserOnlineQueryHandler;
+            _logger = logger;   
+         }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("CASFDASDASD");
+             var accountId = Guid.Parse(context.HttpContext.Request.Query["accountId"].First());
+            var nonce = long.Parse(context.HttpContext.Request.Query["nonce"].First());
+
+            IsUserOnlineQuery isUserOnlineQuery = new IsUserOnlineQuery(accountId, nonce);
+
+            var isUserOnlineResult = _isUserOnlineQueryHandler.Handle(isUserOnlineQuery);
+
+            if (isUserOnlineResult.IsUserOnlineQueryResultStatus == IsUserOnlineQueryResultStatus.DatabaseIssues)
+            {
+                context.Result = new StatusCodeResult(500);
+                return;
+            }
+            else if(isUserOnlineResult.IsUserOnlineQueryResultStatus == IsUserOnlineQueryResultStatus.UserNotFound)
+            {
+                context.Result = new StatusCodeResult(404);
+                return;
+            }
+            else if (isUserOnlineResult.IsUserOnlineQueryResultStatus == IsUserOnlineQueryResultStatus.UserNotLoggedIn)
+            {
+                context.Result = new StatusCodeResult(403); 
+                return;
+            }
+
+
+        }
+ 
+    }
+}
