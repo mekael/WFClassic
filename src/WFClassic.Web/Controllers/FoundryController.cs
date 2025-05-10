1318f59a3a40956bfd7dafc5c599e643915932b3
Michael Hannigan <michael.vincent.hannigan@gmail.com>
2025-05-10 17:36:44 -0500

    fix: add endpoint protection to foundry

--- src/WFClassic.Web/Controllers/FoundryController.cs
+++ src/WFClassic.Web/Controllers/FoundryController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Text.Json;
+using WFClassic.Web.Logic.Foundry.Claim;
 using WFClassic.Web.Logic.Foundry.Pending;
 using WFClassic.Web.Logic.Foundry.Start;
 using WFClassic.Web.Logic.Friendship.Get;
@@ -14,18 +15,21 @@ namespace WFClassic.Web.Controllers
     {
         CheckPendingRecipesQueryHandler _checkPendingRecipesQueryHandler;
         StartRecipeBuildHandler _startRecipeBuildHandler;
+        ClaimCompletedRecipeHandler _claimCompletedRecipeHandler;
         ILogger<FoundryController> _logger;
 
         public FoundryController(ILogger<FoundryController> logger, CheckPendingRecipesQueryHandler checkPendingRecipesQueryHandler,
-            StartRecipeBuildHandler startRecipeBuildHandler )
+            StartRecipeBuildHandler startRecipeBuildHandler, ClaimCompletedRecipeHandler claimCompletedRecipeHandler)
         {
             _logger = logger;
             _checkPendingRecipesQueryHandler = checkPendingRecipesQueryHandler;
             _startRecipeBuildHandler = startRecipeBuildHandler;
+            _claimCompletedRecipeHandler = claimCompletedRecipeHandler;
         }
 
         [HttpPost]
         [Route("/api/instantCompleteRecipe.php")]
+        [TypeFilter(typeof(LoginVerificationActionFilter))]
         public ActionResult InstantCompleteRecipe([FromQuery] Guid accountId, [FromQuery] long nonce)
         {
             _logger.LogInformation("RecipeController => in action InstantCompleteRecipe");
@@ -34,19 +38,35 @@ namespace WFClassic.Web.Controllers
 
         [HttpPost]
         [Route("/api/claimCompletedRecipe.php")]
-        public ActionResult ClaimCompletedRecipe([FromQuery] Guid accountId, [FromQuery] long nonce)
+        [TypeFilter(typeof(LoginVerificationActionFilter))]
+        public ActionResult ClaimCompletedItem([FromQuery]ClaimCompletedRecipe claimCompletedRecipe)
         {
-            _logger.LogInformation("RecipeController => in action claimCompletedRecipe");
-            return new JsonResult("{}");
+
+            _logger.LogInformation("RecipeController => in action startRecipe");
+            var result = _claimCompletedRecipeHandler.Handle(claimCompletedRecipe);
+
+            if (result.ClaimCompletedRecipeResultStatus == ClaimCompletedRecipeResultStatus.DatabaseErrors ||
+                result.ClaimCompletedRecipeResultStatus == ClaimCompletedRecipeResultStatus.MappingFailure)
+            {
+                return StatusCode(500);
+            }
+            else if (result.ClaimCompletedRecipeResultStatus == ClaimCompletedRecipeResultStatus.ValidationErrors)
+            {
+                return BadRequest();
+            }
+            else if (result.ClaimCompletedRecipeResultStatus == ClaimCompletedRecipeResultStatus.Success)
+            {
+                return new JsonResult(result.BuiltItem);
+            }
+
+            return StatusCode(500);
         }
 
         [HttpPost]
         [Route("/api/startRecipe.php")]
+        [TypeFilter(typeof(LoginVerificationActionFilter))]
         public ActionResult StartRecipe([FromQuery] StartRecipeBuild startRecipeBuild)
         {
-            //   Utils.GetRequestObject<string>(this.HttpContext);
-            // Seems like the array is always empty ? 
-  
 
             _logger.LogInformation("RecipeController => in action startRecipe");
             var result = _startRecipeBuildHandler.Handle(startRecipeBuild);
