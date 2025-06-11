4f94d1f9111a9be8655e383f010fb880a9e1fa94
Michael Hannigan <michael.vincent.hannigan@gmail.com>
2025-06-10 19:00:57 -0500

    fix: link to sns in event
    the link to sns was pointing to an out of date discord invite. Update the record to use openwf.io

---
+++ src/WFClassic.Web/Data/Migrations/20250610235840_UpdateOpenWFEvent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFClassic.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOpenWFEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                                    update WorldStateEventMessages 
+                                    set Message = 'Check out Space Ninja Server!',
+                                    Property = 'https://openwf.io/'
+                                    where Id = '31DACE59-46AA-4491-B1CD-9E98147B0D08';
+                                    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
