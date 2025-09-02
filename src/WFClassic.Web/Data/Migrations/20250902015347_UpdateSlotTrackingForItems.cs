691623a8c3fc2df79c258cf5f08ac8ed44b815f7
Michael Hannigan <michael.vincent.hannigan@gmail.com>
2025-09-01 21:11:18 -0500

    feat: update foundry based warframe item defs so that unique items remove slots when utilized.

---
+++ src/WFClassic.Web/Data/Migrations/20250902015347_UpdateSlotTrackingForItems.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFClassic.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSlotTrackingForItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+update WarframeItemComponents 
+set AddInventoryBin=1,
+InventoryBinTypeToAdd = 
+case when ItemType  like '/Lotus/Powersuits%' then 0
+when ItemType  like '/Lotus/Weapons%' then 1
+when ItemType  like '/Lotus/Types/Sentinels/SentinelPowersuits%' then 3
+else 0 
+end,
+NumberOfBinsToAdd = case when ItemType  like '/Lotus/Powersuits%' then -1
+when ItemType  like '/Lotus/Weapons%' then -1
+when ItemType  like '/Lotus/Types/Sentinels/SentinelPowersuits%' then -2
+else 0 
+end
+where id in 
+(
+select wic.id
+from WarframeItems wi 
+inner join warframeitemcomponents wic on wi.id=wic.WarframeItemId
+where wi.WarframeItemLocation =0
+and 
+(wic.ItemType  like '/Lotus/Powersuits%'
+or wic.ItemType  like '/Lotus/Weapons%'
+or wic.ItemType  like '/Lotus/Types/Sentinels/SentinelPowersuits%'
+)
+and wic.ItemType not like '%Card'
+)
+
+
+
+");
+
+
+
+
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
