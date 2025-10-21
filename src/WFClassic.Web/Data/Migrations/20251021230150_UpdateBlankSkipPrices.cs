7c2a974e3f103737ab4c08ca33d8a01ebffe66b5
Michael Hannigan <michael.vincent.hannigan@gmail.com>
2025-10-21 18:07:40 -0500

    fix: according to the data pulled from the actual cache files, a recipe (by default) has a skip price of 25 plat, which can then be overriden in a recipe definition . Recipes have a base which then a specific recipe can inherit from which sets the sale and skip price to sane(ish) defaults. For multiple recipes (specifically the warframe components) the skip price was set to 0 instead of the default which was causing the plat amount for a user not to be decremented while rushing a recipe.
    ```
        "data": {
            "SellingPrice": 2500,
            "Amount": [
                1,
                1
            ],
            "SkipBuildTimePrice": 25
        }
    ```

---
+++ src/WFClassic.Web/Data/Migrations/20251021230150_UpdateBlankSkipPrices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFClassic.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBlankSkipPrices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                                update recipes
+                                set SkipBuildPriceInPlatinum  =25
+                                where SkipBuildPriceInPlatinum  =0
+                                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
