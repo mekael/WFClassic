259f4b9921b2e536a1e5b74849d56b6babc79b83
Michael Hannigan <michael.vincent.hannigan@gmail.com>
2025-10-21 17:42:25 -0500

    fix: update booster prices and set CanUsePlatinum to true as without this we cannot purchase any booster (CanUseCredits was also set to false)

---
+++ src/WFClassic.Web/Data/Migrations/20251021222225_UpdateBoosterCosts.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFClassic.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBoosterCosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                            update MarketPackageDefinitions  
+                            set 
+                                CanBePurchasedWithPlat=1,
+                                CostInPlat = 40,
+                                CostInCredits = 0
+                            where 
+                                ItemType  in(
+                                            '/Lotus/Types/Boosters/AffinityBooster',
+                                            '/Lotus/Types/Boosters/CreditBooster',
+                                            '/Lotus/Types/Boosters/ReviveBooster'
+                                            );
+
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
